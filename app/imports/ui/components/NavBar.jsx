2b7e30492f9898a8b7a9a6ec542e63ad926c1409
kobyv <Kobyv@hawaii.edu>
2021-04-23 20:52:41 -1000

    Added Profile

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -19,7 +19,9 @@ class NavBar extends React.Component {
                     [//<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Menu.Item>,
                         <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Appointments</Menu.Item>,
                         //<Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Menu.Item>,
-                        <Menu.Item as ={NavLink} activeClassName="active" exact to ="/map" key='map'>Map</Menu.Item>]
+                      <Menu.Item as={NavLink} activeClassName="active" exact to="/profile" key='list'>Profile</Menu.Item>,
+
+                      <Menu.Item as ={NavLink} activeClassName="active" exact to ="/map" key='map'>Map</Menu.Item>]
                 ) : ''}
                 {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
                     <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
