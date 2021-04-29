18af65fef20c9721c220340397b0257e6d65519d
Braden Betz <bradenb@hawaii.edu>
2021-04-28 23:02:21 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -36,8 +36,8 @@ class NavBar extends React.Component {
           ) : (
             <Dropdown id="navbar-current-user" text={this.props.currentUser} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
-                <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
                 <Dropdown.Item id="navbar-sign-out" icon="sign out" text="View Profile" as={NavLink} exact to="/profile"/>
+                <Dropdown.Item id="navbar-sign-out" icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
               </Dropdown.Menu>
             </Dropdown>
           )}
