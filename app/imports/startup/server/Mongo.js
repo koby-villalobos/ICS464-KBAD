95bd2d039c4db3ebf2628080ef545b50585a3b06
Auadrian <auadrian@hawaii.edu>
2021-04-30 09:29:23 -1000

    Digits 3
    Finished digits 3 except for admin role

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import {Contacts} from '../../api/contact/Contacts.js';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -9,10 +9,23 @@ function addData(data) {
   Stuffs.collection.insert(data);
 }
 
+function addContact(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.collection.insert(data);
+}
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the StuffsCollection if empty.
+if (Contacts.collection.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default contact.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
   }
 }
